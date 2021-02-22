d6e831e27567553e7f79a118ee87f34c7afe6856
highjune <highjune37@gmail.com>
2021-02-22 20:36:48 +0900

    <feat> : <readExcel 메서드- 콘솔로만 출력>

--- src/main/java/com/hospital/baronic/service/PatientService.java
+++ src/main/java/com/hospital/baronic/service/PatientService.java
@@ -6,13 +6,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class PatientService {
-
 
     @Autowired
     private PatientRepository patientRepository;
