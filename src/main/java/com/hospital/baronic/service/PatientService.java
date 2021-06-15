fb23611c68c1aa4311cd8f416fb8a0e8e6c041ae
highjune <highjune37@gmail.com>
2021-06-15 18:34:48 +0900

    completed refactoring

--- src/main/java/com/hospital/baronic/service/PatientService.java
+++ src/main/java/com/hospital/baronic/service/PatientService.java
@@ -1,14 +1,14 @@
 package com.hospital.baronic.service;
 
-import com.hospital.baronic.domain.patient.Patient;
 import com.hospital.baronic.mapper.PatientMapper;
 import com.hospital.baronic.web.dto.PatientSaveRequestDto;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.io.FileInputStream;
 
 @Service
 public class PatientService {
@@ -19,14 +19,95 @@ public class PatientService {
         this.patientMapper = patientMapper;
     }
 
-    //엑셀 환자 데이터 저장
-    public void insertPatientInfo(int chart_Id, String name) throws Exception {
-        PatientSaveRequestDto patientSaveRequestDto = new PatientSaveRequestDto(chart_Id, name);
-        patientMapper.insertPatientInfo(patientSaveRequestDto);
+    // 엑셀데이터 삽입
+    public String insertPatientInfo() {
+        String ans = "";
+
+        try {
+            // 나중에 파일이름 자동으로 할 때
+//            Date nowDate = new Date();
+//            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMdd");
+//            String strNowDate = simpleDateFormat.format(nowDate);
+//            String fileName = strNowDate + "_진료일검색.xls";
+
+            // 수동 파일이름(이전 날짜든지 ..)
+//            FileInputStream file = new FileInputStream("C:\\Users\\highj\\OneDrive\\바탕 화면\\baronic\\20210115_진료일검색.xls"); // 집pc 경로
+            FileInputStream file = new FileInputStream("C:\\Users\\user\\Desktop\\baronic\\20210115_진료일검색.xls"); // 회사pc 경로
+            XSSFWorkbook workbook = new XSSFWorkbook(file);
+
+            int rowindex = 0;
+            int columnindex = 0;
+
+            XSSFSheet sheet = workbook.getSheetAt(0); // 시트 수 (첫번째에만 존재하므로 0을 준다), 만약 각 시트를 읽기 위해서는 for문을 한번 더 돌려준다.
+            // 행의 수
+            int rows = sheet.getPhysicalNumberOfRows(); // 229행
+//            for(rowindex=0 ; rowindex<rows; rowindex++) { // 0행~228행(총 229행)
+            for (rowindex = 1; rowindex < rows; rowindex++) { // 1행~228행(총 228행) - 1행부터 읽기시작 (0행 제외)
+                // 행 읽기 시작
+                XSSFRow row = sheet.getRow(rowindex);
+                if (row != null) {
+
+                    int chart_Id = -1;
+                    double temp = 0;
+                    String name = "";
+
+//                    int cells = row.getPhysicalNumberOfCells(); // 전체 cell 구할 때
+//                    for(columnindex=0; columnindex<=cells; columnindex++) {
+                    for (columnindex = 2; columnindex <= 3; columnindex++) { // 2열부터 3열(cells) 까지만
+                        // 셀 값을 읽는다.
+                        XSSFCell cell = row.getCell(columnindex);
+                        String value = "";
+                        // 셀이 빈 값일 경우를 위한 널체크
+                        if (cell == null) {
+                            continue;
+                        } else {
+                            // 타입별로 내용 읽기
+                            switch (cell.getCellType()) {
+                                case XSSFCell.CELL_TYPE_FORMULA:
+                                    value = cell.getCellFormula();
+                                    break;
+                                case XSSFCell.CELL_TYPE_NUMERIC:
+                                    value = cell.getNumericCellValue() + "";
+                                    break;
+                                case XSSFCell.CELL_TYPE_STRING:
+                                    value = cell.getStringCellValue() + "";
+                                    break;
+                                case XSSFCell.CELL_TYPE_BLANK:
+                                    value = cell.getBooleanCellValue() + "";
+                                    break;
+                                case XSSFCell.CELL_TYPE_ERROR:
+                                    value = cell.getErrorCellValue() + "";
+                                    break;
+                            }
+                            if (columnindex % 2 == 0) { // 짝수
+//                                chartId = Integer.parseInt(value);
+//                                chartId = (int)value;
+                                temp = Double.parseDouble(value); // "18.0" -> 18.0
+                                chart_Id = Integer.parseInt(String.valueOf(Math.round(temp)));
+//                                System.out.println("chartId = " + chartId);
+
+                            } else { // 홀수
+                                name = value;
+                            }
+                            System.out.println(rowindex + "번 행 : " + columnindex + "번 열 값은 : " + value);
+                        }
+                    } // 안 for문 끝. 2~3열
+//                    this.patientService.insertPatientInfo(chart_Id, name);
+                    PatientSaveRequestDto patientSaveRequestDto = new PatientSaveRequestDto(chart_Id, name);
+                    patientMapper.insertPatientInfo(patientSaveRequestDto);
+
+                } else {
+                    String row_null_error = "row = null";
+                    return row_null_error;
+                }
+            } // 바깥 for문 끝. 228행
+
+            ans = "success";
+            return ans;
+        } catch (Exception e) {
+            e.printStackTrace();
+            ans = "failure";
+            return ans;
+        }
     }
-
-//    public List<Patient> getAllReservations() {
-//        return this.patientRepository.findAll();
-//    }
-
 }
--- src/main/java/com/hospital/baronic/service/ReservationService.java
+++ src/main/java/com/hospital/baronic/service/ReservationService.java
@@ -4,9 +4,16 @@ import com.hospital.baronic.domain.Reservation.Reservation;
 import com.hospital.baronic.mapper.ReservationMapper;
 import com.hospital.baronic.web.dto.ReservationSaveRequestDto;
 import lombok.RequiredArgsConstructor;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.FileInputStream;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -15,18 +22,180 @@ public class ReservationService {
 
     private final ReservationMapper reservationMapper;
 
+    @Autowired
     public ReservationService(ReservationMapper reservationMapper) {
         this.reservationMapper = reservationMapper;
     }
 
-    // 엑셀 예약 데이터 저장
-    public void insertReservationSchedule(ReservationSaveRequestDto reservationSaveRequestDto) throws Exception {
-        this.reservationMapper.insertReservationExcelData(reservationSaveRequestDto);
+    public String parseReservationExcelData() {
+        String ans = "";
+        try {
+            // TODO 병원에서 생성되는 파일 PATH 넣기
+//            Date todayDate = new Date();
+////            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMdd");
+////            String yyyyMMdd = simpleDateFormat.format(todayDate);
+////            String directoryPath = "C:\\Users\\highj\\OneDrive\\바탕 화면\\baronic\\"; // 파일의 위치
+////            String reservationExcelFileName = "예약내역(" + yyyyMMdd + ").xls"; //  파일 이름
+////            FileInputStream file = new FileInputStream(directoryPath + reservationExcelFileName);
+
+            // TODO) 파일 이름 하드 코딩(해당하는 날짜로 이름 붙이기)
+//            FileInputStream file = new FileInputStream("C:\\Users\\highj\\OneDrive\\바탕 화면\\baronic\\예약내역(20210110).xls"); // 집pc 경로
+            FileInputStream file = new FileInputStream("C:\\Users\\user\\Desktop\\baronic\\예약내역(20210110).xls"); // 회사pc 경로
+
+            XSSFWorkbook workbook = new XSSFWorkbook(file);
+
+            int columnindex = 0;
+            XSSFSheet sheet = workbook.getSheetAt(0); // 시트 수 (첫번째에만 존재하므로 0), 만약 각 시트를 읽기 위해서는 for문으로 더.
+
+            XSSFRow firstRow = sheet.getRow(0);
+            int firstRowLength = firstRow.getPhysicalNumberOfCells(); // 열의 총 갯수(A~H, 총 8개)
+
+            String reservation_date = ""; // reservation_date = day_arr + time_arr (ex. "01/10(일요일)" + "오전 10:00")
+
+            ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            // 1) 날짜(day, "01/10일요일", "01/11(월요일)", ...) 먼저 list에 담기
+            ArrayList<String> reservation_day_list = new ArrayList<String>(); // "01/10(일요일)", "01/11(월요일)", ...
+            for (int i = 1 ; i <= firstRowLength ; i++) { // 열의 총 갯수
+//                XSSFRow row = sheet.getRow(0); // 0행만 먼저 읽음
+                String reservation_day = "";
+                if (firstRow != null) {
+                    XSSFCell cell = firstRow.getCell(i); // 첫번째 cell("시간") 제외라서 i=1부터
+                    if (cell == null) {
+                        continue;
+                    } else {
+                        switch(cell.getCellType()) {
+                            case XSSFCell.CELL_TYPE_FORMULA:
+                                reservation_day = cell.getCellFormula(); break;
+                            case XSSFCell.CELL_TYPE_NUMERIC:
+                                reservation_day = cell.getNumericCellValue()+""; break;
+                            case XSSFCell.CELL_TYPE_STRING:
+                                reservation_day = cell.getStringCellValue()+""; break;
+                            case XSSFCell.CELL_TYPE_BLANK:
+                                reservation_day = cell.getBooleanCellValue()+""; break;
+                            case XSSFCell.CELL_TYPE_ERROR:
+                                reservation_day = cell.getErrorCellValue()+""; break;
+                        }
+                    }
+                }
+                reservation_day_list.add(reservation_day); // 담기
+            } // 첫 행(날짜들)만 읽는 for문 끝
+
+            ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            // 2) 시간(time, "오전 10:00") list에 담기
+            ArrayList<String> reservation_time_list = new ArrayList<String>();
+
+            int rows = sheet.getPhysicalNumberOfRows();
+            for(int i = 1; i < rows ; i++){
+                XSSFRow row = sheet.getRow(i);
+                XSSFCell cell = row.getCell(0);
+
+                String reservation_time = "";
+                if (cell == null){
+                    continue;
+                } else {
+                    switch (cell.getCellType()) {
+                        case XSSFCell.CELL_TYPE_FORMULA:
+                            reservation_time = cell.getCellFormula();
+                            break;
+                        case XSSFCell.CELL_TYPE_NUMERIC:
+                            reservation_time = cell.getNumericCellValue() + "";
+                            break;
+                        case XSSFCell.CELL_TYPE_STRING:
+                            reservation_time = cell.getStringCellValue() + "";
+                            break;
+                        case XSSFCell.CELL_TYPE_BLANK:
+                            reservation_time = cell.getBooleanCellValue() + "";
+                            break;
+                        case XSSFCell.CELL_TYPE_ERROR:
+                            reservation_time = cell.getErrorCellValue() + "";
+                            break;
+                    }
+                }
+                reservation_time_list.add(reservation_time); // 담기
+            }
+
+            ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            // 3) DB입력. 1)+2) 로 날짜+시간 만들어서 content랑 같이 DB 삽입
+
+            // 바깥 for문 시작(열)
+            for (columnindex = 1; columnindex <= firstRowLength; columnindex++) {
+                int rowindex = 1;
+
+                String patient_name = "";
+                String todo = "";
+                int chart_id = -1;
+                String dump = "";
+                int position = 0;
+
+                // 안쪽 for문 시작(행)
+                for (; rowindex < rows ; rowindex++) { // 총 90행
+                    XSSFRow row = sheet.getRow(rowindex); // 0행 제외하고 1행부터 시작
+                    XSSFCell cell = row.getCell(columnindex);
+
+                    String reservation_cell_data = "";
+                    if (cell == null) {
+                        position++;
+                        continue;
+                    } else {
+                        // 타입별로 내용 읽기
+                        switch (cell.getCellType()) {
+                            case XSSFCell.CELL_TYPE_FORMULA:
+                                reservation_cell_data = cell.getCellFormula();
+                                break;
+                            case XSSFCell.CELL_TYPE_NUMERIC:
+                                reservation_cell_data = cell.getNumericCellValue() + "";
+                                break;
+                            case XSSFCell.CELL_TYPE_STRING:
+                                reservation_cell_data = cell.getStringCellValue() + "";
+                                break;
+                            case XSSFCell.CELL_TYPE_BLANK:
+                                reservation_cell_data = cell.getBooleanCellValue() + "";
+                                break;
+                            case XSSFCell.CELL_TYPE_ERROR:
+                                reservation_cell_data = cell.getErrorCellValue() + "";
+                                break;
+                        }
+                        System.out.println(columnindex + "번 열 " + rowindex + "번 행 값은 : " + reservation_cell_data);
+                    }
+
+                    reservation_date = reservation_day_list.get(columnindex-1) + " " + reservation_time_list.get(rowindex-1); // 01/10(일요일) 오전10:00
+                    SimpleDateFormat noYearDateFormat = new SimpleDateFormat("MM/dd(E) ahh:mm");
+                    Date dateTypeReservation_date  = noYearDateFormat.parse(reservation_date);
+
+                    // 엑셀데이터에서 chart_id와 to do를 파싱하기 위한 함수
+                    Reservation inputReservationData = ReservationService.parsingReservationExcelDumpData(reservation_cell_data);
+                    patient_name = inputReservationData.getPatient_name();
+                    chart_id = inputReservationData.getChart_id();
+                    todo = inputReservationData.getTodo();
+                    dump = inputReservationData.getDump();
+
+                    System.out.println("===========================================================");
+                    System.out.println("chart_id : " + chart_id + ", patient_name : " + patient_name + ", todo : " + todo + ", dump : " +  dump + ", reservation_date : " +  dateTypeReservation_date);
+                    System.out.println("===========================================================");
+
+                    ReservationSaveRequestDto reservationSaveRequestDto = new ReservationSaveRequestDto(patient_name, chart_id, todo, dump, dateTypeReservation_date, position);
+                    this.reservationMapper.insertReservationExcelData(reservationSaveRequestDto);
+                    // this.reservationService.insertReservationSchedule(patient_name, chart_id, todo, dump, dateTypeReservation_date, position);
+
+                    position++;
+                    if (!reservation_time_list.get(rowindex).equals(reservation_time_list.get(rowindex-1))){
+                        position = 0 ;
+                    }
+
+                } // 안쪽 for문 끝
+
+            } // 바깥 for문 끝
+            ans = "success";
+            return ans;
+        } catch (Exception e) {
+            e.printStackTrace();
+            ans = "failure";
+            return ans;
+        }
     }
 
     // 엑셀데이터에서 chart_id와 to do를 파싱하기 위한 함수
-    public Reservation parsingReservationExcelDumpData(String dumpData) {
-
+    public static Reservation parsingReservationExcelDumpData(String dumpData) {
         int chart_id = -1;
         String patient_name = "";
         String todo = "";
